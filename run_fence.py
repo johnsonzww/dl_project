1f1508e1cb3de3859c9454c294ddb90d507c865b
johnsonzww <z929527511@outlook.com>
2021-05-18 20:00:36 +0800

    remove dup pic

--- run_fence.py
+++ run_fence.py
@@ -11,12 +11,12 @@ OPTIMIZATION_STEPS = 200
 
 # run online optimization
 os.system(
-    'python3 train_fence_online.py --train_dir ' + TRAIN_DIR + ' --training_data_path ' + TRAINING_DATA_PATH  + ' --GPU_ID ' + GPU_ID + ' --max_steps ' + str(
+    'python36 train_fence_online.py --train_dir ' + TRAIN_DIR + ' --training_data_path ' + TRAINING_DATA_PATH  + ' --GPU_ID ' + GPU_ID + ' --max_steps ' + str(
         OPTIMIZATION_STEPS + 10))
 
 # inference with the optimized weights
 os.system(
-    'python3 test_fence.py --test_dataset_name ' + TRAINING_DATA_PATH + '/' + TRAINING_SCENE + ' --img_type ' + IMG_TYPE + ' --ckpt_path ' + TRAIN_DIR + 'model.ckpt-' + str(
+    'python36 test_fence.py --test_dataset_name ' + TRAINING_DATA_PATH + '/' + TRAINING_SCENE + ' --img_type ' + IMG_TYPE + ' --ckpt_path ' + TRAIN_DIR + 'model.ckpt-' + str(
         OPTIMIZATION_STEPS) + ' --output_dir ' + OUTPUT_DIR)
 
 # os.system(
